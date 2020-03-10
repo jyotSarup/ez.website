ff8c0ca0c276b51dac9c65ddcd112bbddee27d71
Heloysa Tancredo <heloysatancredo@Heloysas-MacBook-Pro.local>
2020-03-09 19:38:20 -0700

    Drawer added

--- src/Components/theme.js
+++ src/Components/theme.js
@@ -4,15 +4,23 @@ import { cyan, grey, red } from '@material-ui/core/colors';
 
 
 const theme = createMuiTheme({
-    palette: {
-      primary: cyan,
-      secondary: grey,
-      background: red,
-      action: {
-        active: cyan,
-      },
-    },
-  });
+  palette: {
+    primary: cyan,
+    secondary: grey,
+  },
+  status: {
+    danger: "red"
+  },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 450,
+      md: 600,
+      lg: 900,
+      xl: 1200
+    }
+  },
+});
 
   export default theme;
 
