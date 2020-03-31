37322f3971a590e8a9e2d4285aa07d477fd4553e
Heloysa Tancredo <heloysatancredo@Heloysas-MacBook-Pro.local>
2020-03-31 19:25:12 -0300

    changed team photos for fast loading

--- src/Components/Pages/Team/team.js
+++ src/Components/Pages/Team/team.js
@@ -45,70 +45,70 @@ export default function Team() {
             link: "heloysa-guerreiro",
             detail: "PM and Front-end Developer",
             info: '"My experience includes 6 years working as Business Analyst and Project Manager. In the last 2 years, I’ve been dedicated to gaining in-deep knowledge of systems development."',
-            imgThumb: "heloysa.png"
+            imgThumb: "heloysa.jpg"
         },
         {
             title: "Walter",
             link: "wbgneto",
             detail: "Lead Full Stack Developer",
             info: '"I love to build products, acting strongly in the technical area, which is my specialization, but also in the whole business structure. I am passionate about giving great user experiences to clients."',
-            imgThumb: "walter.png"
+            imgThumb: "walter.jpg"
         },
         {
             title: "Jyot",
             link: "jyotsarupkaur",
             detail: "Front-end Developer",
             info: '"I am a web and mobile application developer with 3+ years of industry experience. I am in this industry because I have always had keen interest in critical thinking and analysis. My goal is to keep doing this by developing business solutions."',
-            imgThumb: "jyot.png"
+            imgThumb: "jyot.jpg"
         },
         {
             title: "Zeinab",
             link: "nazaninahmadi",
             detail: "Backend Developer",
             info: '"I started my professional journey 8 years ago by computer networking and running servers, in between I implemented an ERP system and got familiar with Databases. Now I am a junior web application developer."',
-            imgThumb: "zeinab.png"
+            imgThumb: "zeinab.jpg"
         },
         {
             title: "Saebom",
             link: "saebompak",
             detail: "Front-end Developer",
             info: '"Familiar with writing HTML5, CSS and front-end development technologies such as jQuery, Bootstrap and have related project experience. I am passionate about mycraft."',
-            imgThumb: "saebom.png"
+            imgThumb: "saebom.jpg"
         },
         {
             title: "Saebeyol",
             link: "saebyeolpak",
             detail: "Front-end Developer",
             info: '"I worked as a UI designer for three years and experienced projects using HTML, CSS and jQuery. I love to learn new technology and keep track of lessons learned!"',
-            imgThumb: "saebyeol.png"
+            imgThumb: "saebyeol.jpg"
         },
         {
             title: "Sweera",
             link: "sweerachahal",
             detail: "Front-end Developer",
             info: '"I graduated as a Computer Science Engineer from India and decided to follow my passion of front end development in my higher studies. I love learning about new technologies and creating web applications from a scratch."',
-            imgThumb: "sweera.png"
+            imgThumb: "sweera.jpg"
         },
         {
             title: "Harry",
             link: "harryvtran",
             detail: "Lead UI/UX Designer",
             info: '"I graduated with Degree of Information Technology and have experience working in business (Pharmacy, houseware). I enjoy designing and have an open mindset to learn and to contribute to the team."',
-            imgThumb: "harry.png"
+            imgThumb: "harry.jpg"
         },
         {
             title: "Anukampa",
             link: "anukampa-sharma",
             detail: "UI/UX Designer",
             info: '"I had completed my bachelor’s degree in Computer Science & Engineering. Currently I’m pursuing web and mobile application designing Course."',
-            imgThumb: "anukampa.png"
+            imgThumb: "anukampa.jpg"
         },
         {
             title: "Kuldeeep",
             link: "kuldeep-bhangu",
             detail: "UI Designer",
             info: '"I have done my bachelor\'s in Computer Applications. I enjoy spending my time on designing and always have a positive and flexible attitude towards others. In this project, my contribution as UX and UI designer."',
-            imgThumb: "kuldeep.png"
+            imgThumb: "kuldeep.jpg"
         },
 
     ];
