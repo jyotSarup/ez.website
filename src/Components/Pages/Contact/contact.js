badfbfa181817407cc98c4ec967e250801696ae1
sweera <sweerachahal95@gmail.com>
2020-04-02 20:33:34 -0700

    contact form works properly now

--- src/Components/Pages/Contact/contact.js
+++ src/Components/Pages/Contact/contact.js
@@ -20,7 +20,7 @@ class Contact extends React.Component {
         this.setState(prevState => {
             return {
                 ...prevState,
-                message: "Thank You for your interest!! We will get in touch with you soon." 
+                message: "Thank You for your interest!! We will get in touch with you soon."
             }
         });
         setTimeout(() => {
@@ -31,16 +31,16 @@ class Contact extends React.Component {
                     userName: "",
                     email: "",
                     company: "",
-                    userMsg: "", 
+                    userMsg: "",
                 }
             });
         }, 4000);
     }
-    
+
     componentDidMount() {
         window.scrollTo(0, 0);
     }
-    
+
     handleChange = (event) => {
         const { name, value } = event.target;
         this.setState(prevState => {
@@ -50,7 +50,7 @@ class Contact extends React.Component {
             }
         });
     }
-    
+
     render() {
         return (
 
@@ -82,7 +82,7 @@ class Contact extends React.Component {
                         <TextField variant="outlined" className="contact_input" multiline rowsMax="5" type="text" onBlur="this.value=''" name="userMsg" value={this.state.userMsg} onChange={this.handleChange}></TextField>
 
                         <button type="submit" value="Send" className="contact_button" >Send</button>
- 
+
                         <div className="thanks">
 
                             <h5>{this.state.message}</h5>
@@ -92,12 +92,14 @@ class Contact extends React.Component {
                 </div>
                 <div className="contact_circular_shape">
                 </div>
-          </div>
-       
+            </div>
 
-    );
-}
+
+        );
+    }
 }
 
 
 export default Contact;
+
+//References: https://material-ui.com/
